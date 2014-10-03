6d2300111da2817f73d9cd51dce006d087b290e9
fransonsr <fransonsr@familysearch.org>
2014-10-03 10:38:37 -0600

    Initial api-schema

---
+++ api-schema.sql
@@ -0,0 +1,20 @@
+-- users with role --
+select u.username, :roleName from users u, user_roles ur, roles r
+where u.user_id = ur.user_id
+and ur.role_id = r.role_id
+and r.name = :roleName
+order by u.username;
+
+-- user's role --
+select r.name, :userName from roles r, user_roles ur, users u
+where r.role_id = ur.role_id
+and ur.user_id = u.user_id
+and u.username = :userName
+order by r.name;
+
+-- add role to user --
+insert into user_roles (user_id, role_id)
+values (
+  (select user_id from users where username = :userName),
+  (select role_id from roles where name = :roleName)
+);  
